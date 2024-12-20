c128ab255a8354c794460b16d3c2b99d207693d1
Dooho Yi <pilotedeguerre@gmail.com>
2024-12-20 21:28:15 +0900

    update

--- Assets/Scripts/PieceTriggerHandler.cs
+++ Assets/Scripts/PieceTriggerHandler.cs
@@ -21,18 +21,17 @@ public class PieceTriggerHandler : MonoBehaviour
         //player switch
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            object[] args = {"player1", 1};
+            object[] args = {"playersource", 1};
             pdPatch.SendList("player", args);
-            Debug.Log("1번 키 입력: pdPatch.SendList() 호출됨");
         }
         else if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            object[] args = {"player2", 1};
+            object[] args = {"playersource", 2};
             pdPatch.SendList("player", args);
         }
         else if (Input.GetKeyDown(KeyCode.Alpha3))
         {
-            object[] args = {"player3", 1};
+            object[] args = {"playersource", 3};
             pdPatch.SendList("player", args);
         }
 
@@ -49,55 +48,55 @@ public class PieceTriggerHandler : MonoBehaviour
     {
         if (other.CompareTag("Stair"))
         {
-            Debug.Log("Stair!!" + uBuild.stairCount);
+            Debug.Log("Enter Stair!! [" + uBuild.stairCount + "]");
             object[] args = {"Stair", 1, uBuild.stairCount};
             pdPatch.SendList(receiver, args);
         }
         if (other.CompareTag("Fluid"))
         {
-            Debug.Log("Fluid!!");
+            Debug.Log("Enter Fluid!!");
             object[] args = {"Fluid", 1, uBuild.fluidCount};
             pdPatch.SendList(receiver, args);
         }
         if (other.CompareTag("Rubble"))
         {
-            Debug.Log("Rubble!!");
+            Debug.Log("Enter Rubble!!");
             object[] args = {"Rubble", 1, uBuild.rubbleCount};
             pdPatch.SendList(receiver, args);
         }
         if (other.CompareTag("Slope"))
         {
-            Debug.Log("Slope!!");
+            Debug.Log("Enter Slope!!");
             object[] args = {"Slope", 1, uBuild.slopeCount};
             pdPatch.SendList(receiver, args);
         }
         if (other.CompareTag("SpeedArea"))
         {
-            Debug.Log("SpeedArea!!");
+            Debug.Log("Enter SpeedArea!!");
             object[] args = {"SpeedArea", 1, uBuild.speedAreaCount};
             pdPatch.SendList(receiver, args);
         }
         if (other.CompareTag("Terrain"))
         {
-            Debug.Log("Terrain!!");
+            Debug.Log("Enter Terrain!!");
             object[] args = {"Terrain", 1, uBuild.terrainCount};
             pdPatch.SendList(receiver, args);
         }
         if (other.CompareTag("Fog"))
         {
-            Debug.Log("Fog!!");
+            Debug.Log("Enter Fog!!");
             object[] args = {"Fog", 1, uBuild.fogCount};
             pdPatch.SendList(receiver, args);
         }
         if (other.CompareTag("Grass"))
         {
-            Debug.Log("Grass!!");
+            Debug.Log("Enter Grass!!");
             object[] args = {"Grass", 1, uBuild.grassCount};
             pdPatch.SendList(receiver, args);
         }
         if (other.CompareTag("Tree"))
         {
-            Debug.Log("Tree!!");
+            Debug.Log("Enter Tree!!");
             object[] args = {"Tree", 1, uBuild.treeCount};
             pdPatch.SendList(receiver, args);
         }
@@ -107,50 +106,55 @@ public class PieceTriggerHandler : MonoBehaviour
     {
         if (other.CompareTag("Stair"))
         {
-            Debug.Log("Excit!!");
+            Debug.Log("Exit Stair!!");
             object[] args = {"Stair", 0, uBuild.stairCount};
             pdPatch.SendList(receiver, args);
         }
         if (other.CompareTag("Fluid"))
         {
+            Debug.Log("Exit Fluid!!");
             object[] args = {"Fluid", 0, uBuild.fluidCount};
             pdPatch.SendList(receiver, args);
         }
         if (other.CompareTag("Rubble"))
         {
+            Debug.Log("Exit Rubble!!");
             object[] args = {"Rubble", 0, uBuild.rubbleCount};
             pdPatch.SendList(receiver, args);
         }
         if (other.CompareTag("Slope"))
         {
-            object[] args = {"Slope", 0};
-            pdPatch.SendList(receiver, args, uBuild.slopeCount);
+            Debug.Log("Exit Slope!!");
+            object[] args = {"Slope", 0, uBuild.slopeCount};
+            pdPatch.SendList(receiver, args);
         }
         if (other.CompareTag("SpeedArea"))
         {
+            Debug.Log("Exit SpeedArea!!");
             object[] args = {"SpeedArea", 0, uBuild.speedAreaCount};
             pdPatch.SendList(receiver, args);
         }
         if (other.CompareTag("Terrain"))
         {
+            Debug.Log("Exit Terrain!!");
             object[] args = {"Terrain", 0, uBuild.terrainCount};
             pdPatch.SendList(receiver, args);
         }
         if (other.CompareTag("Fog"))
         {
-            Debug.Log("Fog!!");
+            Debug.Log("Exit Fog!!");
             object[] args = {"Fog", 0, uBuild.fogCount};
             pdPatch.SendList(receiver, args);
         }
         if (other.CompareTag("Grass"))
         {
-            Debug.Log("Grass!!");
+            Debug.Log("Exit Grass!!");
             object[] args = {"Grass", 0, uBuild.grassCount};
             pdPatch.SendList(receiver, args);
         }
         if (other.CompareTag("Tree"))
         {
-            Debug.Log("Tree!!");
+            Debug.Log("Exit Tree!!");
             object[] args = {"Tree", 0};
             pdPatch.SendList(receiver, args, uBuild.treeCount);
         }
